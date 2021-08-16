cc4bc909302b2b593b4b46dc77e48b895fee706b
Durva123 <84019341+Durva123@users.noreply.github.com>
2021-08-16 17:05:43 +0530

    Add files via upload

---
+++ game_page.js
@@ -0,0 +1,30 @@
+player_name=localStorage.getItem("player1_name");
+player_name=localStorage.getItem("player2_name");
+player1_score=0;
+player1_score=0;
+document.getElementById("player1_name").innerHTML=player1_name+" : ";
+document.getElementById("player2_name").innerHTML=player2_name+" : ";
+document.getElementById("player1_score").innerHTML=player1_score;
+document.getElementById("player2_score").innerHTML=player2_score;
+document.getElementById("player_question").innerHTML="Question Turn -"+player1_name;
+document.getElementById("player_answer").innerHTML="Answer Turn -"+player2_name;
+
+function send(){
+    get_word=document.getElementById("word").value;
+    word=get_word.toLowerCase();
+    console.log(word);
+    charat1=word.charAt(1);
+    console.log(charat1);
+    divide_2=Math.floor(word.lenght/2);
+    charat2=word.charAt(divide_2);
+lenght_minus_1=word.lenght-1;
+charat3=word.charAt(lenght_minus_1);
+removecharat1=word.replace(charat1,"_");
+removecharat2=removecharat1.replace(charat2,"_");
+removecharat3=removecharat2.replace(charat3,"_");
+question_word="<h4 id='word_explain'>Q."+removecharat3+"</h4>;
+input_box="<br>Answer: <input type='text' id='input_ckeck_box'>";
+check_button="<br><br><button class='btn btn-info' onclick='check()'>Check</button>";
+row=question_word+input_box+check_buttom;
+document.getElementById("output").innerHTML=row;
+document.getElementById("word").value="";
